53589f14d7cd4be700859f6c6518aa69b9e4b267
Aditya Ranjan <aditya74810@gmail.com>
2025-07-11 13:00:13 +0530

    Uodated MovieCard

--- src/app/components/MovieCard.js
+++ src/app/components/MovieCard.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import styles from '@/app/styles/common.module.css'
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import styles from "@/app/styles/common.module.css";
+import Image from "next/image";
+import Link from "next/link";
 const MovieCard = (curElem) => {
-    const { id, type, title, synopsis } = curElem.jawSummary;
-    return (
-        <>
-            <div className={styles.card}>
-                <div className={styles.card_image}>
-                {
-                (curElem.jawSummary.backgroundImage.url)?
-                    <Image src={curElem.jawSummary.backgroundImage.url} alt={title} width={260} height={200} />:
-                    <Image src='/hero.svg' alt="default image" width={200} height={200}/>
-                }
-                </div>
-                <div className={styles.card_data}>
-                    <h2>{title.substring(0, 18)}</h2>
-                    <p>{`${synopsis.substring(0, 66)}...`}</p>
-                    <Link href={`/movie/${id}`}>
-                        <button>
-                            Read More
-                        </button>
-                    </Link>
-                </div>
-            </div>
+  const { id, type, title, synopsis } = curElem.jawSummary;
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.card_image}>
+          {curElem.jawSummary.backgroundImage.url ? (
+            <img
+              src={curElem.jawSummary.backgroundImage.url}
+              alt={title}
+              width={260}
+              height={200}
+            />
+          ) : (
+            <img
+              src="/hero.svg"
+              alt="default image"
+              width={200}
+              height={200}
+            />
+          )}
+        </div>
+        <div className={styles.card_data}>
+          <h2>{title.substring(0, 18)}</h2>
+          <p>{`${synopsis.substring(0, 66)}...`}</p>
+          <Link href={`/movie/${id}`}>
+            <button>Read More</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default MovieCard
+export default MovieCard;
--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -44,3 +44,55 @@ const Movie = async () => {
 }
 
 export default Movie
+
+// app/movie/page.tsx or app/movie/index.tsx (depending on your routing)
+// import React from 'react'
+// import MovieCard from '../components/MovieCard'
+// import styles from '@/app/styles/common.module.css'
+
+// const Movie = async () => {
+//   const url = 'https://netflix54.p.rapidapi.com/search/?query=series&offset=0&limit_titles=50&limit_suggestions=20&lang=en'; // Sample endpoint
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': process.env.RAPID_API_KEY,  // make sure this is set in your .env file
+//       'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+//     }
+//   }
+
+//   try {
+//     const res = await fetch(url, options);
+
+//     if (!res.ok) {
+//       throw new Error(`HTTP error! Status: ${res.status}`);
+//     }
+
+//     const data = await res.json();
+//     const main_data = data.titles;
+
+//     return (
+//       <section className={styles.movieSection}>
+//         <div className={styles.container}>
+//           <h1>Series & Movie</h1>
+//           <div className={styles.card_section}>
+//             {main_data.map((curElem) => (
+//               <MovieCard key={curElem.id} {...curElem} />
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     );
+//   } catch (error) {
+//     console.error('Failed to fetch movies:', error);
+
+//     return (
+//       <section className={styles.movieSection}>
+//         <div className={styles.container}>
+//           <h1>Failed to load movies. Please try again later.</h1>
+//         </div>
+//       </section>
+//     );
+//   }
+// };
+
+// export default Movie;
